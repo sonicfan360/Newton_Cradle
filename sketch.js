4c8af86dae0924b0f067c70ea444ea0c410838ee
sonicfan360 <72156017+sonicfan360@users.noreply.github.com>
2020-11-21 10:41:39 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+    ball = new paper(200,200,30)
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ball.display();
+  drawSprites();
+ 
+}
+
+
+
